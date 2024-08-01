d3fbab110c2fcefe6089f97f7113af780685e9a5
Yeonjae Choi <fox2yj@naver.com>
2024-08-01 18:04:48 +0900

    [FEAT] User 모델 생성

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True, blank=False)
+    email = models.EmailField(max_length=254, unique=True, blank=False)
+    password = models.CharField(max_length=128, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)  # Automatically set the timestamp when created
+
+    def __str__(self):
+        return self.username
--- petever/settings.py
+++ petever/settings.py
@@ -108,10 +108,16 @@ AWS_DEFAULT_ACL = None
 
 # Static and media files
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
-MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+#MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
