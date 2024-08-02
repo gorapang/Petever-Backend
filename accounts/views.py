0592ffb5bff80fb9c637274ac811ce402c2475f4
Yeonjae Choi <fox2yj@naver.com>
2024-08-02 12:27:35 +0900

    [FEAT] register users, user list 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import UserSerializer
+from .models import *
 
-# Create your views here.
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
