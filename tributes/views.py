ac87a1442ac86ec10d9e3ec654fd6318eb9e4da0
jeongju <wnwjdqkr@gmail.com>
2024-08-03 15:43:48 +0900

    [FEAT] 추모관 리스트 기능 수정

--- tributes/views.py
+++ tributes/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
-from .serializers import MemorialSerializer, GalleryImageSerializer, FootprintSerializer
+from .serializers import MemorialSerializer, GalleryImageSerializer, FootprintSerializer, MemorialListSerializer
 from rest_framework.viewsets import ModelViewSet
 from . models import Memorial, GalleryImage, Footprint
+from rest_framework.parsers import MultiPartParser, FormParser
+
+# class MemorialViewSet(ModelViewSet):
+#     queryset = Memorial.objects.all()
+#     serializer_class = MemorialSerializer
 
 class MemorialViewSet(ModelViewSet):
     queryset = Memorial.objects.all()
-    serializer_class = MemorialSerializer
+    parser_classes = (MultiPartParser, FormParser)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return MemorialListSerializer
+        return MemorialSerializer
+    
 
 class GalleryImageViewSet(ModelViewSet):
     queryset = GalleryImage.objects.all()
