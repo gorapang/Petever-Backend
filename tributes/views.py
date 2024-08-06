66dfffec70073504959ea20b6bbaa7c79eb6fbdb
unknown <wnwjdqkr@gmail.com>
2024-08-06 11:37:44 +0900

    [FEAT] 특정 유저의 추모관 조회 기능 추가

--- tributes/views.py
+++ tributes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .serializers import MemorialSerializer, GalleryImageSerializer, FootprintSerializer, MemorialListSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 from .models import Memorial, GalleryImage, Footprint
 from rest_framework.parsers import MultiPartParser, FormParser
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class MemorialViewSet(ModelViewSet):
     queryset = Memorial.objects.all()
@@ -21,6 +23,20 @@ class MemorialViewSet(ModelViewSet):
         if self.action == 'create':
             self.permission_classes = [IsAuthenticated]
         return super(MemorialViewSet, self).get_permissions()
+
+    @action(detail=False, methods=['get'], url_path='user/(?P<user_id>[^/.]+)')
+    def user_memorials(self, request, user_id=None):
+        """
+        특정 유저가 생성한 memorial 조회`
+        """
+        user = get_object_or_404(User, id=user_id)
+        memorials = self.queryset.filter(user=user)
+        page = self.paginate_queryset(memorials)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(memorials, many=True)
+        return Response(serializer.data)
 
 
 class GalleryImageViewSet(ModelViewSet):
